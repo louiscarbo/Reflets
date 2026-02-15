8a9e27a9918625131f1a610f049aac916a9feb46
Louis Carbo Estaque <louis@carbo-estaque.fr>
2026-02-15 10:07:03 +0100

    Clean image entity generation files

--- Models/ARObjectComponents/Generators/ImageGenerator.swift
+++ Models/ARObjectComponents/Generators/ImageGenerator.swift
@@ -7,6 +7,7 @@
 
 import RealityKit
 import Foundation
+import UIKit
 
 struct ImageGenerator: EntityGeneratorStrategy {
     func generateEntity(for object: ARObject) -> Entity {
@@ -26,5 +27,38 @@ struct ImageGenerator: EntityGeneratorStrategy {
         }
         
         return entity
+    }
+}
+
+private extension ImageGenerator {
+    func createTextureFromPNG(image: UIImage) -> (TextureResource?, Float)? {
+        let aspectRatio = Float(image.size.width / image.size.height)
+        guard let cgImage = image.cgImage else { return nil }
+        
+        do {
+            let texture = try TextureResource(image: cgImage, options: .init(semantic: .normal))
+            return (texture, aspectRatio)
+        } catch {
+            print("Error creating texture from image: \(error)")
+            return nil
+        }
+    }
+    
+    func create2DEntityFromImage(image: UIImage, size: Float, opacity: Double = 1.0) -> Entity {
+        guard let (texture, aspectRatio) = createTextureFromPNG(image: image) else {
+            return Entity()
+        }
+        
+        let width: Float = 0.5 * size
+        let height: Float = width / aspectRatio
+        let planeMesh = MeshResource.generatePlane(width: width, height: height)
+        
+        var material = UnlitMaterial()
+        material.color = .init(tint: UIColor.white.withAlphaComponent(CGFloat(opacity)), texture: .init(texture!))
+        material.opacityThreshold = 0.01
+        material.faceCulling = .none
+        
+        let modelEntity = ModelEntity(mesh: planeMesh, materials: [material])
+        return modelEntity
     }
 }
